2d8736eaf17ba7bfcdbfd9c17c7c684f774ebcf4
Gustavo Delfim <gusttavodelfim@gmail.com>
2023-02-17 14:05:03 -0300

    first commit

---
+++ jest.config.js
@@ -0,0 +1,24 @@
+const nextJest = require('next/jest')
+
+const createJestConfig = nextJest({
+  dir: './',
+})
+
+/** @type {import('jest').Config} */
+const customJestConfig = {
+  coverageProvider: 'v8',
+
+  setupFilesAfterEnv: [
+    '<rootDir>/jest.setup.js'
+  ],
+
+  moduleDirectories: ['node_modules', '<rootDir>/'],
+
+  moduleNameMapper: {
+    '@/(.*)$': '<rootDir>/src/$1',
+  },
+
+  testEnvironment: 'jest-environment-jsdom',
+}
+
+module.exports = createJestConfig(customJestConfig)
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,12 @@
-import './globals.css'
+import { ReactNode } from 'react'
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+type RootLayoutProps = {
+  children: ReactNode
+}
+
+export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
       <head />
       <body>{children}</body>
     </html>
